f6ab2503db1e25c798ada42c0221f286013770d1
Kurados <89411136+Kurados@users.noreply.github.com>
2023-03-07 15:25:50 +0800

    fixed untracked files

--- RFSM/Library/PackageCache/com.unity.toonshader@0.8.5-preview/Runtime/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("Unity.ToonShader.Editor")]
-[assembly: InternalsVisibleTo("Unity.VisualCompositor")]
-[assembly: InternalsVisibleTo("Unity.VisualCompositor.Tests")]
-[assembly: InternalsVisibleTo("Unity.VisualCompositor.Editor")]
-[assembly: InternalsVisibleTo("Unity.VisualCompositor.EditorTests")]
-
-
